a45abf5f4b1aa0b1d43fa56cce7be39f4ef405b0
Jeniferks <jeniferkindermann@gmail.com>
2021-12-10 18:56:13 -0300

    Fiz o exercicio 6

--- semana3/revisao-js/lista-revisao-js/lista-revisao-js/exercicios.js
+++ semana3/revisao-js/lista-revisao-js/lista-revisao-js/exercicios.js
@@ -24,72 +24,75 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-    let novoArray = array.filter(pares => (pares%2) === 0);
+    let novoArray = array.filter(pares => (pares % 2) === 0);
     return novoArray
 
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
+    let arrayPares = [];
+    for (const i of array) {
+        if (i % 2 === 0)
+            arrayPares.push(i * i)
+    }
 
-}
+    // EXERCÍCIO 06
+    function retornaMaiorNumero(array) {
 
-// EXERCÍCIO 06
-function retornaMaiorNumero(array) {
+    }
 
-}
+    // EXERCÍCIO 07
+    function retornaObjetoEntreDoisNumeros(num1, num2) {
 
-// EXERCÍCIO 07
-function retornaObjetoEntreDoisNumeros(num1, num2) {
+    }
 
-}
+    // EXERCÍCIO 08
+    function retornaNPrimeirosPares(n) {
 
-// EXERCÍCIO 08
-function retornaNPrimeirosPares(n) {
+    }
 
-}
+    // EXERCÍCIO 09
+    function classificaTriangulo(ladoA, ladoB, ladoC) {
 
-// EXERCÍCIO 09
-function classificaTriangulo(ladoA, ladoB, ladoC) {
+    }
 
-}
+    // EXERCÍCIO 10
+    function retornaSegundoMaiorESegundoMenor(array) {
 
-// EXERCÍCIO 10
-function retornaSegundoMaiorESegundoMenor(array) {
+    }
 
-}
+    // EXERCÍCIO 11
+    function retornaChamadaDeFilme(filme) {
 
-// EXERCÍCIO 11
-function retornaChamadaDeFilme(filme) {
+    }
 
-}
+    // EXERCÍCIO 12
+    function retornaPessoaAnonimizada(pessoa) {
 
-// EXERCÍCIO 12
-function retornaPessoaAnonimizada(pessoa) {
+    }
 
-}
+    // EXERCÍCIO 13A
+    function retornaPessoasAutorizadas(pessoas) {
 
-// EXERCÍCIO 13A
-function retornaPessoasAutorizadas(pessoas) {
+    }
 
-}
+    // EXERCÍCIO 13B
+    function retornaPessoasNaoAutorizadas(pessoas) {
 
-// EXERCÍCIO 13B
-function retornaPessoasNaoAutorizadas(pessoas) {
+    }
 
-}
+    // EXERCÍCIO 14
+    function retornaContasComSaldoAtualizado(contas) {
 
-// EXERCÍCIO 14
-function retornaContasComSaldoAtualizado(contas) {
+    }
 
-}
+    // EXERCÍCIO 15A
+    function retornaArrayOrdenadoAlfabeticamente(consultas) {
 
-// EXERCÍCIO 15A
-function retornaArrayOrdenadoAlfabeticamente(consultas) {
+    }
 
-}
+    // EXERCÍCIO 15B
+    function retornaArrayOrdenadoPorData(consultas) {
 
-// EXERCÍCIO 15B
-function retornaArrayOrdenadoPorData(consultas) {
-
-}
+    }
