1e88676cf099861a8738a21a877cb53607725bbc
Jenifer <jeniferkindermann@gmail.com>
2022-03-11 23:52:06 -0300

    Consegui colocar os videos, porem nao adicionei player nem fiz com api

--- modulo3/projeto-labefy/labefy/src/App.js
+++ modulo3/projeto-labefy/labefy/src/App.js
@@ -1,13 +1,29 @@
-import './App.css';
-import  {MeuComponente} from './components/componente';
-import NavButton from './components/navButtons';
+import  React from 'react';
+import ReactPlayer from 'react-player';
+import './components/videoPlayer.js/videoPlayer.css';
+
+
+
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-      <MeuComponente></MeuComponente>
-      <NavButton/>
+      <ReactPlayer url="https://www.youtube.com/watch?v=vAEwLvxHVVk" controls={true}/>
+      <input type="text" placeholder='player'/>
+      <ReactPlayer url="https://www.youtube.com/watch?v=SDTZ7iX4vTQ" controls={true}/>
+      <input type="text" placeholder='player'/>
+      <ReactPlayer url="https://www.youtube.com/watch?v=pXRviuL6vMY" controls={true}/>
+      <input type="text" placeholder='player'/>
+      <ReactPlayer url="https://www.youtube.com/watch?v=iArXv64tCJA" controls={true}/>
+      <input type="text" placeholder='player'/>
+      <ReactPlayer url="https://www.youtube.com/watch?v=dqUdI4AIDF0" controls={true}/>
+      <input type="text" placeholder='player'/>
+      <ReactPlayer url="https://www.youtube.com/watch?v=8M143iRrbgw" controls={true}/>
+      <input type="text" placeholder='player'/>
+      <ReactPlayer url="https://www.youtube.com/watch?v=TDyiREoBw0o" controls={true}/>
+      <input type="text" placeholder='player'/>
       </header>
     </div>
   ); 
