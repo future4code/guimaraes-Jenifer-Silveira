49912ca81a489cea88197238b698e70686b821e5
Jenifer <jeniferkindermann@gmail.com>
2022-03-14 20:44:03 -0300

    fiz alteraçoes usando components

---
+++ modulo3/projeto-labefy/labefy/src/components/VideoPlayer/VideoPlayer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./VideoPlayer.css"
+
+
+
+export default  function VideoPlayer () {
+    return(
+    <div>
+        <div className="container">
+            <div className="lista-videos">
+                <div className="item">
+                <img src="https://br.web.img3.acsta.net/r_1920_1080/pictures/14/12/16/09/59/112719.jpg" alt=""/>
+                <h2>Video 1</h2>
+                <img src="https://br.web.img3.acsta.net/r_1920_1080/pictures/14/12/16/09/59/112719.jpg" alt=""/>
+                <h2>Video 2</h2>
+                <img src="https://br.web.img3.acsta.net/r_1920_1080/pictures/14/12/16/09/59/112719.jpg" alt=""/>
+                <h2>Video 3</h2>
+                <img src="https://br.web.img3.acsta.net/r_1920_1080/pictures/14/12/16/09/59/112719.jpg" alt=""/>
+                <h2>Video 4</h2>    
+                </div>    
+            </div>    
+        </div>
+    </div>
+        
+    )
+}
+
