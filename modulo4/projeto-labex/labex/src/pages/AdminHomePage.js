b814d5035a051e7ebadaed581bb893e51bbde25c
Jenifer <jeniferkindermann@gmail.com>
2022-04-07 20:04:14 -0300

    criei mais paginas do site

---
+++ modulo4/projeto-labex/labex/src/pages/AdminHomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminHomePage = () => {
+    return(
+        <h1>Pagina administrador</h1>
+    )
+}
+
+export default AdminHomePage
---
+++ modulo4/projeto-labex/labex/src/pages/TripDetailsPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TripDetailsPage = () => {
+    return(
+        <h1>detahes de viagens</h1>
+    )
+}
+
+export default TripDetailsPage
