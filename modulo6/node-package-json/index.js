54921a54b7ab8739cb6be88cf26759deff709f4c
Jenifer <jeniferkindermann@gmail.com>
2022-05-26 17:10:08 -0300

    Alterado package.json para limpar e executar

--- modulo6/node-package-json/index.js
+++ modulo6/node-package-json/index.js
@@ -1,4 +1,6 @@
-const name = process.argv[2]
+//exercicios 1 e 2
+
+/*const name = process.argv[2]
 const age = Number(process.argv[3])
 const futureAge = 7
 
@@ -10,4 +12,8 @@ const num2 = Number(process.argv[5])
 console.log(`A soma dos números é ${num1+num2}`)
 console.log(`A subtraçao dos números é ${num2-num1}`)
 console.log(`A multiplicaçao dos números é ${num1*num2}`)
-console.log(`A divisao dos números é ${num1%num2}`)
+console.log(`A divisao dos números é ${num1%num2}`)*/
+
+//exercicio 3
+
+const listaDeCompras = ["Comprar Feijão"]  
