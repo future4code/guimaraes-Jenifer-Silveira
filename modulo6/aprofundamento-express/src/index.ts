771e5fb434cc551b5cadb3731645afb58622c4fb
Jenifer <jeniferkindermann@gmail.com>
2022-06-07 16:43:16 -0300

    Consegui colocar o servidor em pé

--- modulo6/aprofundamento-express/src/index.ts
+++ modulo6/aprofundamento-express/src/index.ts
@@ -1,17 +1,28 @@
-import express, { Express } from "express";
+import express from "express";
 import cors from "cors";
-import { AddressInfo } from "net";
+//import { AddressInfo } from "net";
+import { Request, Response } from "express";
 
-const app: Express = express();
-
+const app = express();
 app.use(express.json());
 app.use(cors());
 
-const server = app.listen(process.env.PORT || 3003, () => {
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send("Deu certo")
+
+})
+
+app.listen(3003, () => {
+    console.log("servidor em pé")
+})
+/*
+const server = app.listen(process.env.PORT || 3000, () => {
     if(server) {
         const address = server.address() as AddressInfo;
         console.log(`Server is running in http://localhost: ${address.port}`)
     } else {
         console.error(`Failure upon starting server.`)
     }
-})
+})*/
