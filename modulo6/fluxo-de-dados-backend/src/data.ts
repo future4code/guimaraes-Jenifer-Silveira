53bea188bf9c308216d8fc1a493258d86e97d661
Jenifer <jeniferkindermann@gmail.com>
2022-06-07 23:42:55 -0300

    Criei arquivo data.ts

---
+++ modulo6/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,17 @@
+export const produtos = [
+    {
+        id: "1",
+        name: "Café",
+        price: Number("8,00") 
+    },
+    {
+        id: "2",
+        name: "Açucar",
+        price: Number("4,00") 
+    },
+    {
+        id: "3",
+        name: "Leite",
+        price: Number("5,00") 
+    },
+]
---
+++ modulo6/fluxo-de-dados-backend/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors"
+import { AddressInfo } from "net";
+import { Request, Response} from "express"
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).send("Hello world!!!")
+})
+
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+    console.error(`Failure upon starting server.`);
+    }
+});
+
