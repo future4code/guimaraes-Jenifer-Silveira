958298880f47534d6334f832612fb2cf16b07e77
Jenifer <jeniferkindermann@gmail.com>
2022-06-21 22:04:25 -0300

    Query com defeito

--- modulo6/knex-js/src/index.ts
+++ modulo6/knex-js/src/index.ts
@@ -10,14 +10,14 @@ app.post("/actor" , async (req:Request, res:Response) => {
     const {name, birth_date, salary, gender} = req.body;
 
     try {
-        await connection.raw(`INSERT INTO (id, name, birth_date, salary, gender)  Actor 
-        VALUES(
+        await connection.raw(`INSERT INTO Actor  (id, name, birth_date, salary, gender)  
+        VALUES (
             7,
             \" ${name}\",
             \"${birth_date}\",
             ${Number(salary)},
-            \"${gender}\",
-        )`);
+            \"${gender}\"
+        );`);
     } catch (error) {
         console.log({error})
         return  res.status(500).send("Algo deu errado")
