7a00cb4dacb08da898ec7204c04d48faf49ab07c
Jenifer <jeniferkindermann@gmail.com>
2022-07-12 23:10:44 -0300

    classes-encapsulamento

--- modulo6/classes-encapsulamento/src/index.ts
+++ modulo6/classes-encapsulamento/src/index.ts
@@ -3,29 +3,87 @@ import app from "./app";
 import { Request, Response } from "express"
 
 app.get("/test", (req: Request, res: Response) => {
-    res.status(200).send("Okkkkkkkk")
+    res.status(200).send("Ok!")
 })
 
 
 //Exercicio 1
 //a) Serve para determinar qual parametro será recebido (string, number, boolean)
+//c) Através do constructor 
 
+class Transaction {
+    private description: string;
+    private value: number;
+    private date: string;
+
+    constructor(
+        description: string,
+        value: number,
+        date: string,
+    ){
+        this.description = description;
+        this.value = value;
+        this.date = date;
+    }
+}
 class UserAccount {
     private cpf: string;
     private name: string;
     private age: number;
     private balance: number = 0;
-    //private transactions: Transaction[] = [];
+    private transactions: Transaction[] = [];
 
     constructor(
         cpf: string,
         name: string,
         age: number,
+        balance: number,
     ) {
-        console.log("Chamando o construtor da classe UserAccount")
+        
         this.cpf = cpf;
         this.name = name;
         this.age = age;
+        this.balance = balance;
+    }
+
+    public getCpf(): string{
+        return this.cpf
+    }
+
+    public getName(): string{
+        return this.name
+    }
+
+    public getAge(): number{
+        return this.age
+    }
+
+    
+    public getBalance() : number{
+        return this.balance
+    }
+
+    public getTransaction() : Transaction[] {
+        return this.transactions
+    }
+
+    public setBalance (value: number): void {
+        this.balance += value
+    }
+
+    public setTransactions(newBalance: Transaction):void{
+        this.transactions.push(newBalance)
     }
 
 }
+//const userInfo = new UserAccount("088.178.089-53","Jenifer Kindermann", 30);
+
+const newBalance  = new UserAccount("088.178.089-53","Jenifer", 30, 1000)
+
+console.log(newBalance)
+
+
+
+
+
+
