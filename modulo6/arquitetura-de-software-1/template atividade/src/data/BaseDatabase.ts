3e26865b3f4cf76ef01e30747c1b7ccec931dad1
Jenifer <jeniferkindermann@gmail.com>
2022-07-28 20:05:37 -0300

    endpoint createUser criado

--- modulo6/arquitetura-de-software-1/template atividade/src/data/BaseDatabase.ts
+++ modulo6/arquitetura-de-software-1/template atividade/src/data/BaseDatabase.ts
@@ -5,13 +5,14 @@ dotenv.config()
 
 export class BaseDatabase {
     protected static connection = knex({
-       client: "mysql",
-       connection: {
+        client: "mysql",
+        connection: {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: 3306
-     }
+        database: process.env.DB_SCHEMA,
+        port: 3306,
+        multipleStatements: true
+    }
     })
 }
--- modulo6/arquitetura-de-software-1/template atividade/src/index.ts
+++ modulo6/arquitetura-de-software-1/template atividade/src/index.ts
@@ -1,6 +1,9 @@
 import {app} from "./app"
+import { UserController } from "./controller/UserController"
 
+const userController = new UserController()
 
+app.post("/createUser" , userController.createUser)
 
 app.get("/", async function(){
    console.log("endpoint teste")
